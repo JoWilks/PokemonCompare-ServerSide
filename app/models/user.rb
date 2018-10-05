a9fd04fb7fdc2a41c9329d6c29a20766ec07a515
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-10-05 11:42:50 +0100

    Validate added username uniqueness, Jo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, uniqueness: { case_sensitive: false }
+    validates :username, uniqueness: { case_sensitive: false }, presence: true
 
     has_many :pokemons
     has_many :poketeams
